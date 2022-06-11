77cb4a6ac817dc580cf332697dcb67661921e444
dwmorrell <davismorrell@gmail.com>
2022-06-11 13:48:19 -0600

    first commit

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgres://flfyupzf:RUrTNfDWXDO5s1wYZ1CrbYz8Wz3Je4Dl@heffalump.db.elephantsql.com/flfyupzf",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
