1cf194ad39ab31764236f8548ca1e737f8cf611c
dwmorrell <davismorrell@gmail.com>
2022-06-21 10:11:26 -0600

    first commit

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const { PORT = 5000 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
+const express = require('express');
+const cors = require('cors')
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
